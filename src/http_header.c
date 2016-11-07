8f29f5842c4436fd032ae504137cf9da2b2d7ee0
Joe Groocock <frebib@gmail.com>
2016-11-07 23:52:33 +0000

    Add default headers the server should always send

--- src/http_header.c
+++ src/http_header.c
@@ -23,3 +23,52 @@ http_header_t* find_header(http_header_t* headers, char* key) {
   }
   return NULL;
 }
+
+http_header_t* default_headers() {
+  http_header_t* headers = malloc(sizeof(http_header_t));
+  headers->next               = malloc(sizeof(http_header_t));
+  headers->next->next         = malloc(sizeof(http_header_t));
+  headers->next->next->next   = NULL;
+
+  date_head(headers);
+  serv_head(headers->next);
+  conn_close_head(headers->next->next);
+
+  return headers;
+}
+
+int date_head(http_header_t* header) {
+  if (header == NULL)
+    return -1;
+
+  size_t len = 128;
+  char* buf = malloc(len);
+  time_t now = time(0);
+  struct tm time = *gmtime(&now);
+  strftime(buf, len, "%a, %d %b %Y %H:%M:%S %Z", &time);
+
+  header->name = strdup("Date");
+  header->value = buf;
+
+  return 0;
+}
+
+int serv_head(http_header_t* header) {
+  if (header == NULL)
+    return -1;
+
+  header->name = strdup("Server");
+  header->value = strdup(SERVER_NAME_VER);
+
+  return 0;
+}
+
+int conn_close_head(http_header_t* header) {
+  if (header == NULL)
+    return -1;
+
+  header->name = strdup("Connection");
+  header->value = strdup("close");
+
+  return 0;
+}
--- src/http_header.h
+++ src/http_header.h
@@ -1,6 +1,8 @@
 #ifndef _HTTP_HEADER
 #define _HTTP_HEADER
 
+#define SERVER_NAME_VER "Webserver v0.1"
+
 typedef struct http_header {
     char* name;
     char* value;
@@ -11,5 +13,12 @@ void free_head(http_header_t* headers);
 
 http_header_t* find_header(http_header_t* headers, char* key);
 
+http_header_t* default_headers();
+
+int date_head(http_header_t* header);
+
+int serv_head(http_header_t* header);
+
+int conn_close_head(http_header_t* header);
 
 #endif //WEBSERVER_HTTP_HEADER_H
