3956128d67f0c6c13cceae861319e420eb4cab7c
Joe Groocock <frebib@gmail.com>
2016-11-07 14:11:40 +0000

    Fix memory leak when failing to bind socket

--- src/net.c
+++ src/net.c
@@ -20,17 +20,20 @@ int bindSocket(int port) {
   int ret = getaddrinfo(NULL, port_chr, &hints, &info);
   if (ret != 0) {
     fprintf(stderr, "getaddrinfo: %s", gai_strerror(ret));
+    freeaddrinfo(info);
     return -1;
   }
 
   // Open a TCP stream socket
   if ((sock = socket(info->ai_family, info->ai_socktype, info->ai_protocol)) == -1) {
     fprintf(stderr, "Failed to open socket: %s\n", strerror(errno));
+    freeaddrinfo(info);
     return -1;
   }
   // Ensure SO_REUSEADDR is set
   if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
     fprintf(stderr, "Failed to set socket option SO_REUSEADDR: %s\n", strerror(errno));
+    freeaddrinfo(info);
     return -1;
   }
 
@@ -40,6 +43,7 @@ int bindSocket(int port) {
     if (errno == EACCES) {
       fprintf(stderr, "You should run this program as root to bind the port %d or use a port >= 1024\n", port);
     }
+    freeaddrinfo(info);
     return -1;
   }
   freeaddrinfo(info);
